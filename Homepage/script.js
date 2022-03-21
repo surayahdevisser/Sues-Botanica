0690afd8bf1709da3702f3c832aa3b8687d9a00a
Surayah <81412512+surayahdevisser@users.noreply.github.com>
2022-03-22 00:00:15 +0100

    Finished the homepage content
    Finished creating the content of the homepage, added links to the contact page and vice versa, adjusted the style of the contact page.

--- Homepage/script.js
+++ Homepage/script.js
@@ -12,24 +12,14 @@ window.addEventListener("scroll", function () {
 
 
 
-// increases the size of the social media icons on hover
-
-document.addEventListener("mouseover" ,function () {
-   
-    const instagram = document.getElementsById("instagram");
-    const facebook = document.getElementsById("facebook");
-
-    // instagram.classList.toggle("fa-5x");
-    //facebook.classList.toggle("fa-5x");
 
 
-    //if(mouseover == true) {
 
-    instagram.classList.remove("fa-4x");
-    instagram.classList.add("fa-5x");
-    facebook.classList.remove("fa-4x");
-    facebook.classList.add("fa-5x");
+// calculates current year for copyright line
 
-    //}
+const date = new Date();
+const year = date.getFullYear();
 
-})
+document.getElementById("year").innerHTML = year;
+
+
